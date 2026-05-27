6a327498554ea3660398da796d312903ae09fa5d
Jesús Baquiax <jesusbaquiax13@gmail.com>
2026-05-27 11:03:42 -0400

    Update csv_parser.py

--- backend/parsers/csv_parser.py
+++ backend/parsers/csv_parser.py
@@ -3,3 +3,9 @@ import pandas as pd
 df = pd.read_csv('sample_data\sample_data.csv')
 
 print(df)
+
+expenses = df[df["amount"] < 0]
+
+total_spent = expenses["amount"].sum()
+
+print(f"total amount spent: {total_spent}:.2f")
