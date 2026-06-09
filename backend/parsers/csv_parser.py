e267d8f5342a7b5b4c6e95fe1521915565192e38
Jesús Baquiax <jesusbaquiax13@gmail.com>
2026-06-09 09:32:46 -0400

    adding categorize_transaction function

--- backend/parsers/csv_parser.py
+++ backend/parsers/csv_parser.py
@@ -1,11 +1,47 @@
 import pandas as pd
+import json
 
-df = pd.read_csv('sample_data\sample_data.csv')
 
-print(df)
+def categorize_transaction(description: str) -> str:
+    description_upper = description.upper()
 
-expenses = df[df["amount"] < 0]
+    with open("config/category_rules.json") as f:
+        CATEGORY_RULES = json.load(f)
 
-total_spent = expenses["amount"].sum()
+    for category, keywords in CATEGORY_RULES.items():
+        for keyword in keywords:
+            if keyword in description_upper:
+                return category
 
-print(f"total amount spent: {total_spent}:.2f")
+    return "Uncategorized"
+
+
+def main():
+    df = pd.read_csv("sample_data/sample_statement.csv")
+
+    df["category"] = df["description"].apply(categorize_transaction)
+
+    print("\nCategorized Transactions:")
+    print(df)
+
+    expenses = df[df["amount"] < 0]
+
+    spending_by_category = (
+        expenses
+        .groupby("category")["amount"]
+        .sum()
+        .abs()
+        .sort_values(ascending=False)
+    )
+
+    print("\nSpending by Category:")
+    print(spending_by_category)
+
+    top_category = spending_by_category.index[0]
+    top_amount = spending_by_category.iloc[0]
+
+    print(f"\nTop spending category: {top_category} - ${top_amount:.2f}")
+
+
+if __name__ == "__main__":
+    main()
