66622d23b08c1f8bf33d42ac49dbd7cfd18ab674
Jesús Baquiax <jesusbaquiax13@gmail.com>
2026-06-10 09:10:13 -0400

    added parse_csv func

--- backend/parsers/csv_parser.py
+++ backend/parsers/csv_parser.py
@@ -1,42 +1,9 @@
 import pandas as pd
 from backend.services.categorizer import categorize_transaction
 
-def main():
-    df = pd.read_csv("sample_data/sample_data.csv")
+def parse_csv(file_path: str) -> pd.DataFrame:
+    df = pd.read_csv(file_path)
 
     df["category"] = df["description"].apply(categorize_transaction)
 
-    # print("\nCategorized Transactions:")
-    # print(df)
-
-    expenses = df[df["amount"] < 0]
-
-    total_expenses = expenses["amount"].abs().sum()
-
-    spending_by_category = (
-        expenses
-        .groupby("category")["amount"]
-        .sum()
-        .abs()
-        .sort_values(ascending=False)
-    )
-
-    top_category = spending_by_category.index[0]
-    top_amount = spending_by_category.iloc[0]
-
-    print("\nStatement Summary")
-    print("\nIncome:") 
-    print("$2,200.00")
-
-    print("\nExpenses:")
-    print(f"${total_expenses:.2f}")
-
-    print("\nTop Spending Category:") 
-    print(f"{top_category}: ${top_amount:.2f}")
-
-    print("\nCategory Breakdown:")
-    for category, amount in spending_by_category.items():
-        print(f"{category}: ${amount:.2f}")
-
-if __name__ == "__main__":
-    main()
+    return df
