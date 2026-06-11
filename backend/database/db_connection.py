04f991d959c66dfec4ed14520edb413a5c6c6e80
Jesús Baquiax <jesusbaquiax13@gmail.com>
2026-06-11 10:21:25 -0400

    adding variables

--- backend/database/db_connection.py
+++ backend/database/db_connection.py
@@ -1,13 +1,18 @@
 # Postgres Connection
 
+import os
+
 import psycopg
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def get_connection():
     return psycopg.connect(
-        host="localhost",
-        port=5432,
-        dbname="financial_document_rag",
-        user="postgres",
-        password="YOUR_PASSWORD_HERE"
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
