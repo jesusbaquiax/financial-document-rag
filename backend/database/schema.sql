10476f6128cfeb2e89e99a807a38c7ec2be80049
Jesús Baquiax <jesusbaquiax13@gmail.com>
2026-06-11 10:04:46 -0400

    create transactions  query

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,8 +1,10 @@
-CREATE TABLE transactions (
+-- table creation SQL
+
+CREATE TABLE IF NOT EXISTS transactions (
     transaction_id SERIAL PRIMARY KEY,
-    transaction_date DATE,
-    description VARCHAR(255),
-    amount NUMERIC(12,2),
+    transaction_date DATE NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    amount NUMERIC(12,2) NOT NULL,
     category VARCHAR(100),
     source_file VARCHAR(255),
     created_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
